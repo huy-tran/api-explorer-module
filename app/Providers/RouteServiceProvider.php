cbaf9e2b06da5c00df31e4283fdfd1a808f0efd7
Huy Tran <huy.t@greenhat.net>
2026-03-30 13:58:51 +1000

    Isolate middleware

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,8 +2,13 @@
 
 namespace Modules\ApiExplorer\Providers;
 
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -13,7 +18,12 @@ class RouteServiceProvider extends ServiceProvider
             return;
         }
 
-        Route::middleware('web')
-            ->group(__DIR__.'/../../routes/web.php');
+        Route::middleware(config('api-explorer.middleware', [
+            EncryptCookies::class,
+            AddQueuedCookiesToResponse::class,
+            StartSession::class,
+            ShareErrorsFromSession::class,
+            ValidateCsrfToken::class,
+        ]))->group(__DIR__.'/../../routes/web.php');
     }
 }
